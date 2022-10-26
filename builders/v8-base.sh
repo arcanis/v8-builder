d8df7714d121dcf3e50978f8162b08b93668670d
V8 Builder <v8.builder@localhost>
2022-10-26 14:12:52 +0200

    wip

---
+++ builders/v8-base.sh
@@ -0,0 +1,36 @@
+VERSION=$1
+
+git config --global user.name "V8 Builder"
+git config --global user.email "v8.builder@localhost"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global color.ui true
+
+cd ~
+echo "=====[ Getting Depot Tools ]====="	
+git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$(pwd)/depot_tools:$PATH
+gclient
+
+mkdir v8
+cd v8
+
+echo "=====[ Fetching V8 ]====="
+fetch v8
+echo "target_os = ['$V8_PLATFORM']" >> .gclient
+cd ~/v8/v8
+git checkout $VERSION
+gclient sync
+
+read -r -d '' V8_FLAGS <<- EOM || true
+target_os = "${V8_PLATFORM}"
+$(cat $SCRIPT_DIR/../V8_FLAGS)
+EOM
+
+echo "=====[ Building V8 ]====="
+python ./tools/dev/v8gen.py gen -vv -b "$V8_CPU".release build -- "$V8_FLAGS"
+ninja -C out.gn/build -t clean
+ninja -C out.gn/build v8_monolith
+
+rsync -rv --include="*/" --include="*.h" --exclude="*" ./include/ out.gn/build/gen/include
+ls -Rlh out.gn/build
--- builders/v8-linux-x64.sh
+++ builders/v8-linux-x64.sh
@@ -2,7 +2,7 @@ set -ex
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-V8_PLATFORM=mac
+V8_PLATFORM=linux
 V8_CPU=x64
 
 source "$SCRIPT_DIR"/v8-base.sh
